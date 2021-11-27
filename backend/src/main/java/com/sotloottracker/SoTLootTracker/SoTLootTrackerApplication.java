c64f8305fd34af56e077cc77ab2cc588b343e7cd
BuildTools <unconfigured@null.spigotmc.org>
2021-11-27 19:17:33 +0100

    first commit

---
+++ backend/src/main/java/com/sotloottracker/SoTLootTracker/SoTLootTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sotloottracker.SoTLootTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoTLootTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoTLootTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sotloottracker/SoTLootTracker/SoTLootTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sotloottracker.SoTLootTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoTLootTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
